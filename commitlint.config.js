4701126b98f593da30a62bbfdcd06e02118ff1ca
Cheikh Redouane <cheikh.redouane15@gmail.com>
2021-01-26 17:47:18 +0100

    Ajout de la constance pi

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,7 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 'use strict';
-
+const pi = 3.14;
 module.exports = {
   extends: ['cz'],
   rules: {
